ec29d306535daebca25f819b3ac3238a22347d0d
segi3channel <44390326+segi3channel@users.noreply.github.com>
2018-12-06 06:36:38 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 ///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
 ///Ganti xxxxxxx dengan punya kalian
 ///Capture dulu apk nya ambil yang di butuhkan
-$ticket = 
-"sHp2r312otmCpnGns3mCqJhovNiBiojOutCNbb_bd86xeo6vfXbFyoO2k2G1iHmshHq33IB3i5avmIqiv8uDy62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
-$meid = "2d7c803f6277cf3a";
-$os="android";
-$sign="c06c037cac8959f028955fefed7f6cd438e365b85af381effcbd3688a2b1f3a8";
+$ticket =
+"xxxxxxxxxxx";
+$meid = "xxxxxxxxx";
+$os="xxxxxxxx";
+$sign="xxxxxxxxxx";
